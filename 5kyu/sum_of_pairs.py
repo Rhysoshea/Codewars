c8ddbf353c10e68954886bd9be372c018bdc27a9
rhys <rhys.shea.10@ucl.ac.uk>
2019-09-26 18:36:34 +0900

    sum of pairs

---
+++ 5kyu/sum_of_pairs.py
@@ -0,0 +1,54 @@
+'''
+Instructions
+
+Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+sum_pairs([11, 3, 7, 5],         10)
+#              ^--^      3 + 7 = 10
+== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],         6)
+#          ^-----^         4 + 2 = 6, indices: 0, 2 *
+#             ^-----^      3 + 3 = 6, indices: 1, 3
+#                ^-----^   2 + 4 = 6, indices: 2, 4
+#  * entire pair is earlier, and therefore is the correct answer
+== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)
+#  there are no pairs of values that can be added to produce 2.
+== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#              ^-----------^   5 + 5 = 10, indices: 1, 5
+#                    ^--^      3 + 7 = 10, indices: 3, 4 *
+#  * entire pair is earlier, and therefore is the correct answer
+== [3, 7]
+Negative numbers and duplicate numbers can and will appear.
+
+NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+
+'''
+
+
+''' Solution '''
+def sum_pairs(ints, s):
+    potentials = []
+    for i in range(len(ints)):
+        for j in range(i+1,len(ints)):
+            # print (potentials)
+            if ints[i] + ints[j] == s:
+                # print(i, j)
+                if not potentials:
+                    potentials = [i,j]
+                else:
+                    if j< potentials[1]:
+                        potentials = [i,j]
+    if not potentials:
+        return None
+    return [ints[potentials[0]], ints[potentials[1]]]
+
+l5= [10, 5, 2, 3, 7, 5]
+
+l1= [1, 4, 8, 7, 3, 15]
+
+print(sum_pairs(l1,8))
